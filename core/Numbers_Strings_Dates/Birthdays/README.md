367a4a753179297f2f407dd8f84d827e1ac0bebb
realimp <impinbox@hotmail.com>
2019-12-05 09:19:19 +0300

    Added a bit of structure

---
+++ core/Numbers_Strings_Dates/Birthdays/README.md
@@ -0,0 +1,15 @@
+## The Task
+
+Write an application to print out in console your age and corresponding date and day of week for each of your birthdays.
+
+#### Sample output:
+
+```
+0 - 13.02.1989 - Mon
+1 - 13.02.1990 - Tue
+...
+```
+
+### Additional task
+
+Day of week must be printed in Russian
--- core/Numbers_Strings_Dates/Birthdays/src/Main.java
+++ core/Numbers_Strings_Dates/Birthdays/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         SimpleDateFormat dateFormat = new SimpleDateFormat(" - dd.MM.yyyy - EEEE", Locale.forLanguageTag("ru"));
 
-        Calendar calendar = new GregorianCalendar(1988, Calendar.JULY, 21);
+        Calendar calendar = new GregorianCalendar(1989, Calendar.FEBRUARY, 13);
 
         int i=0;
 

31f8734d07c775929cebbcd6b19ee964b9d3752f
realimp <impinbox@hotmail.com>
2019-12-06 09:12:52 +0300

    Readme added

---
+++ core/Numbers_Strings_Dates/StringExperiments/README.md
@@ -0,0 +1,9 @@
+## The Task
+
+For the given string:
+
+```java
+String text = "John earned 5000 dollars, Pete - 7563 dollars, and Mary - 30000 dollars";
+```
+
+Write a simple console application that will extract and sum earnings of each person using regular expressions. Print out the sum.
--- core/Numbers_Strings_Dates/StringExperiments/src/Loader.java
+++ core/Numbers_Strings_Dates/StringExperiments/src/Loader.java
@@ -6,7 +6,6 @@ public class Loader
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
         String earnings[] = text.replaceAll("[^0-9 \\s]", "").trim().split("\\s+");
-
         int totalEarnings = 0;
 
         for (int i = 0; i < earnings.length; i++)
